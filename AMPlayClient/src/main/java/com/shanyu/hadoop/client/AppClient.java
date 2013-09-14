d2114d7515fdcf121fa94f5131d95c8060fd263f
Shanyu Zhao <shzhao@microsoft.com>
2013-09-13 22:32:31 -0700

    AppClient and AppMaster are buildable now.
    Signed-off-by: Shanyu Zhao <shzhao@microsoft.com>

---
+++ AMPlayClient/src/main/java/com/shanyu/hadoop/client/AppClient.java
@@ -0,0 +1,226 @@
+package com.shanyu.hadoop.client;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.net.NetUtils;
+import org.apache.hadoop.security.SecurityInfo;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.yarn.api.ApplicationClientProtocol;
+import org.apache.hadoop.yarn.api.ApplicationConstants;
+import org.apache.hadoop.yarn.api.protocolrecords.GetApplicationReportRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.GetApplicationReportResponse;
+import org.apache.hadoop.yarn.api.protocolrecords.GetNewApplicationRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.GetNewApplicationResponse;
+import org.apache.hadoop.yarn.api.protocolrecords.SubmitApplicationRequest;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.hadoop.yarn.api.records.ApplicationReport;
+import org.apache.hadoop.yarn.api.records.ApplicationSubmissionContext;
+import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
+import org.apache.hadoop.yarn.api.records.LocalResource;
+import org.apache.hadoop.yarn.api.records.LocalResourceType;
+import org.apache.hadoop.yarn.api.records.LocalResourceVisibility;
+import org.apache.hadoop.yarn.api.records.Resource;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.exceptions.YarnException;
+import org.apache.hadoop.yarn.ipc.YarnRPC;
+import org.apache.hadoop.yarn.security.client.ClientRMSecurityInfo;
+import org.apache.hadoop.yarn.util.ConverterUtils;
+import org.apache.hadoop.yarn.util.Records;
+
+
+/**
+ * AppClient
+ *
+ */
+public class AppClient extends Configured implements Tool {
+  
+  private static final Log LOG = LogFactory.getLog(AppClient.class);
+  
+  private final Configuration conf;
+  private final YarnRPC rpc;
+  private final String amJarUri;
+  
+  private String appName = "AMPlay";
+  private int amMemory = 1024;
+  
+  private ApplicationClientProtocol applicationsManager = null;
+  private ApplicationId appId = null;
+  private ContainerLaunchContext amContainer = null;
+  private ApplicationSubmissionContext appContext = null;
+  private ApplicationReport appReport = null;
+  
+  
+  public AppClient() {
+    conf = getConf();
+    rpc = YarnRPC.create(conf);
+    amJarUri = "";
+  }
+  
+  @Override
+  public int run(String[] args) throws Exception {
+    connect();
+    getAppId();
+    setupAMContainer();
+    setupAppContext();
+    submitApp();
+    
+    return 0;
+  }
+  
+  private void connect() {
+    YarnConfiguration yarnConf = new YarnConfiguration(conf);
+    InetSocketAddress rmAddress = 
+        NetUtils.createSocketAddr(yarnConf.get(
+            YarnConfiguration.RM_ADDRESS,
+            YarnConfiguration.DEFAULT_RM_ADDRESS));             
+    LOG.info("Connecting to ResourceManager at " + rmAddress);
+
+//    Configuration appsManagerServerConf = new Configuration(conf);
+//    appsManagerServerConf.setClass(
+//        YarnConfiguration.YARN_SECURITY_INFO,
+//        ClientRMSecurityInfo.class, SecurityInfo.class);
+
+    applicationsManager = ((ApplicationClientProtocol) rpc.getProxy(
+        ApplicationClientProtocol.class, rmAddress, conf));
+  }
+  
+  private void getAppId() throws IOException,YarnException {
+    GetNewApplicationRequest request = 
+        Records.newRecord(GetNewApplicationRequest.class);              
+    GetNewApplicationResponse response = 
+        applicationsManager.getNewApplication(request);
+    
+    appId = response.getApplicationId();
+    LOG.info("Got new ApplicationId=" + appId);
+  }
+  
+  private void setupAMContainer() throws IOException {
+    // Create a new container launch context for the AM's container
+    amContainer = Records.newRecord(ContainerLaunchContext.class);
+
+    // Define the local resources required 
+    Map<String, LocalResource> localResources = 
+        new HashMap<String, LocalResource>();
+    // Lets assume the jar we need for our ApplicationMaster is available in 
+    // HDFS at a certain known path to us and we want to make it available to
+    // the ApplicationMaster in the launched container 
+    Path jarPath = new Path(amJarUri); // <- known path to jar file
+    FileSystem fs = FileSystem.get(conf);
+    FileStatus jarStatus = fs.getFileStatus(jarPath);
+    LocalResource amJarRsrc = Records.newRecord(LocalResource.class);
+    // Set the type of resource - file or archive
+    // archives are untarred at the destination by the framework
+    amJarRsrc.setType(LocalResourceType.FILE);
+    // Set visibility of the resource 
+    // Setting to most private option i.e. this file will only 
+    // be visible to this instance of the running application
+    amJarRsrc.setVisibility(LocalResourceVisibility.APPLICATION);          
+    // Set the location of resource to be copied over into the 
+    // working directory
+    amJarRsrc.setResource(ConverterUtils.getYarnUrlFromPath(jarPath)); 
+    // Set timestamp and length of file so that the framework 
+    // can do basic sanity checks for the local resource 
+    // after it has been copied over to ensure it is the same 
+    // resource the client intended to use with the application
+    amJarRsrc.setTimestamp(jarStatus.getModificationTime());
+    amJarRsrc.setSize(jarStatus.getLen());
+    // The framework will create a symlink called AppMaster.jar in the 
+    // working directory that will be linked back to the actual file. 
+    // The ApplicationMaster, if needs to reference the jar file, would 
+    // need to use the symlink filename.  
+    localResources.put("AppMaster.jar", amJarRsrc);    
+    // Set the local resources into the launch context    
+    amContainer.setLocalResources(localResources);
+
+    // Set up the environment needed for the launch context
+    Map<String, String> env = new HashMap<String, String>();    
+    // For example, we could setup the classpath needed.
+    // Assuming our classes or jars are available as local resources in the
+    // working directory from which the command will be run, we need to append
+    // "." to the path. 
+    // By default, all the hadoop specific classpaths will already be available 
+    // in $CLASSPATH, so we should be careful not to overwrite it.   
+    String classPathEnv = "$CLASSPATH:./*:";    
+    env.put("CLASSPATH", classPathEnv);
+    amContainer.setEnvironment(env);
+    
+    // Construct the command to be executed on the launched container 
+    String command =
+        "${JAVA_HOME}/bin/java" +
+        " MyAppMaster" + 
+        " arg1 arg2 arg3" + 
+        " 1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout" +
+        " 2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr";                     
+
+    List<String> commands = new ArrayList<String>();
+    commands.add(command);
+    // add additional commands if needed                
+
+    // Set the command array into the container spec
+    amContainer.setCommands(commands);
+  }
+  
+  private void setupAppContext() {
+    appContext = 
+        Records.newRecord(ApplicationSubmissionContext.class);
+    // set the ApplicationId 
+    appContext.setApplicationId(appId);
+    // set the application name
+    appContext.setApplicationName(appName);
+    
+    // Set the container launch content into the ApplicationSubmissionContext
+    appContext.setAMContainerSpec(amContainer);
+    
+    // Define the resource requirements for the container
+    // For now, YARN only supports memory so we set the memory 
+    // requirements. 
+    // If the process takes more than its allocated memory, it will 
+    // be killed by the framework. 
+    // Memory being requested for should be less than max capability 
+    // of the cluster and all asks should be a multiple of the min capability. 
+    Resource capability = Records.newRecord(Resource.class);
+    capability.setMemory(amMemory);
+    appContext.setResource(capability);
+  }
+  
+  private void submitApp() throws IOException,YarnException {
+    // Create the request to send to the ApplicationsManager 
+    SubmitApplicationRequest appRequest = 
+        Records.newRecord(SubmitApplicationRequest.class);
+    appRequest.setApplicationSubmissionContext(appContext);
+
+    // Submit the application to the ApplicationsManager
+    // Ignore the response as either a valid response object is returned on 
+    // success or an exception thrown to denote the failure
+    applicationsManager.submitApplication(appRequest);
+  }
+  
+  private void checkApp() throws IOException,YarnException {
+    GetApplicationReportRequest reportRequest = 
+        Records.newRecord(GetApplicationReportRequest.class);
+    reportRequest.setApplicationId(appId);
+    GetApplicationReportResponse reportResponse = 
+        applicationsManager.getApplicationReport(reportRequest);
+    appReport = reportResponse.getApplicationReport();
+  }
+  
+  
+  public static void main( String[] args ) throws Exception {
+    System.out.println( "Hello World!" );
+    int rc = ToolRunner.run(new Configuration(), new AppClient(), args);
+    System.exit(rc);
+  }
+}
---
+++ AMPlayMaster/src/main/java/com/shanyu/hadoop/master/AppMaster.java
@@ -0,0 +1,163 @@
+package com.shanyu.hadoop.master;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.net.NetUtils;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.yarn.api.ApplicationConstants;
+import org.apache.hadoop.yarn.api.ApplicationMasterProtocol;
+import org.apache.hadoop.yarn.api.protocolrecords.FinishApplicationMasterRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.RegisterApplicationMasterRequest;
+import org.apache.hadoop.yarn.api.protocolrecords.RegisterApplicationMasterResponse;
+import org.apache.hadoop.yarn.api.records.ApplicationAttemptId;
+import org.apache.hadoop.yarn.api.records.ContainerId;
+import org.apache.hadoop.yarn.api.records.FinalApplicationStatus;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.exceptions.YarnException;
+import org.apache.hadoop.yarn.ipc.YarnRPC;
+import org.apache.hadoop.yarn.util.ConverterUtils;
+import org.apache.hadoop.yarn.util.Records;
+
+
+/**
+ * ApplicationMater
+ *
+ */
+public class AppMaster extends Configured implements Tool {
+  
+  private static final Log LOG = LogFactory.getLog(AppMaster.class);
+  
+  private final Configuration conf;
+  private final YarnRPC rpc;
+  
+  private ApplicationAttemptId appAttemptID;
+  private ApplicationMasterProtocol resourceManager;
+  
+  public AppMaster () {
+    conf = getConf();
+    rpc = YarnRPC.create(conf);
+  }
+  
+  @Override
+  public int run(String[] args) throws Exception {
+    getAttemptId();
+    connect();
+    registerAM();
+    
+    //need to use allocate() to emit heartbeat
+    Thread.sleep(30000);
+    
+    finishUp();
+    return 0;
+  }
+  
+  private void getAttemptId() {
+    Map<String, String> envs = System.getenv();
+    String containerIdString = 
+        envs.get(ApplicationConstants.Environment.CONTAINER_ID);
+    if (containerIdString == null) {
+      // container id should always be set in the env by the framework 
+      throw new IllegalArgumentException(
+          "ContainerId not set in the environment");
+    }
+    ContainerId containerId = ConverterUtils.toContainerId(containerIdString);
+    ApplicationAttemptId appAttemptID = containerId.getApplicationAttemptId();
+    LOG.info("ApplicationAttemptId = " + appAttemptID);
+  }
+  
+  private void connect() {
+    YarnConfiguration yarnConf = new YarnConfiguration(conf);
+    InetSocketAddress rmAddress = 
+        NetUtils.createSocketAddr(yarnConf.get(
+            YarnConfiguration.RM_ADDRESS,
+            YarnConfiguration.DEFAULT_RM_ADDRESS));             
+    LOG.info("Connecting to ResourceManager at " + rmAddress);
+
+    resourceManager = (ApplicationMasterProtocol) rpc.getProxy(
+        ApplicationMasterProtocol.class, rmAddress, conf);
+  }
+  
+  private void registerAM() throws IOException,YarnException{
+    InetSocketAddress serviceAddr = null;
+    serviceAddr = new InetSocketAddress("localhost", 8888);
+    
+    // Register the AM with the RM
+    // Set the required info into the registration request: 
+    // ApplicationAttemptId, 
+    // host on which the app master is running
+    // rpc port on which the app master accepts requests from the client 
+    // tracking url for the client to track app master progress
+    RegisterApplicationMasterRequest appMasterRequest = 
+        Records.newRecord(RegisterApplicationMasterRequest.class);
+    //appMasterRequest.setApplicationAttemptId(appAttemptID);     
+    appMasterRequest.setHost(serviceAddr.getHostName());
+    appMasterRequest.setRpcPort(serviceAddr.getPort());
+    appMasterRequest.setTrackingUrl(serviceAddr.getHostName() + ":" + serviceAddr.getPort());
+    
+    // The registration response is useful as it provides information about the 
+    // cluster. 
+    // Similar to the GetNewApplicationResponse in the client, it provides 
+    // information about the min/mx resource capabilities of the cluster that 
+    // would be needed by the ApplicationMaster when requesting for containers.
+    RegisterApplicationMasterResponse response = 
+        resourceManager.registerApplicationMaster(appMasterRequest);
+  }
+  
+  private void finishUp() throws IOException, YarnException {
+    FinishApplicationMasterRequest appFinishRequest = 
+        Records.newRecord(FinishApplicationMasterRequest.class);
+    appFinishRequest.setFinalApplicationStatus(FinalApplicationStatus.SUCCEEDED);
+    resourceManager.finishApplicationMaster(appFinishRequest);
+  }
+  
+  public static void main(String[] args) throws Exception {
+    int rc = ToolRunner.run(new Configuration(), new AppMaster(), args);
+    System.exit(rc);
+  }
+
+  /*
+  private void addResourceRequest(Priority priority, String resourceName,
+      Resource capability) {
+    Map<String, Map<Resource, ResourceRequest>> remoteRequests =
+      this.remoteRequestsTable.get(priority);
+    if (remoteRequests == null) {
+      remoteRequests = new HashMap<String, Map<Resource, ResourceRequest>>();
+      this.remoteRequestsTable.put(priority, remoteRequests);
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("Added priority=" + priority);
+      }
+    }
+    Map<Resource, ResourceRequest> reqMap = remoteRequests.get(resourceName);
+    if (reqMap == null) {
+      reqMap = new HashMap<Resource, ResourceRequest>();
+      remoteRequests.put(resourceName, reqMap);
+    }
+    ResourceRequest remoteRequest = reqMap.get(capability);
+    if (remoteRequest == null) {
+      remoteRequest = recordFactory.newRecordInstance(ResourceRequest.class);
+      remoteRequest.setPriority(priority);
+      remoteRequest.setResourceName(resourceName);
+      remoteRequest.setCapability(capability);
+      remoteRequest.setNumContainers(0);
+      reqMap.put(capability, remoteRequest);
+    }
+    remoteRequest.setNumContainers(remoteRequest.getNumContainers() + 1);
+
+    // Note this down for next interaction with ResourceManager
+    addResourceRequestToAsk(remoteRequest);
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("addResourceRequest:" + " applicationId="
+          + applicationId.getId() + " priority=" + priority.getPriority()
+          + " resourceName=" + resourceName + " numContainers="
+          + remoteRequest.getNumContainers() + " #asks=" + ask.size());
+    }
+  }
+  */
+}
