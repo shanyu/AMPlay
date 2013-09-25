3ed31698c5fa78c699d27034f144806c8e8853e4
Shanyu Zhao <shzhao@microsoft.com>
2013-09-25 11:19:01 -0700

    Add scripts to copy files and run yarn job.
    Signed-off-by: Shanyu Zhao <shzhao@microsoft.com>

---
+++ copyToHdfs.cmd
@@ -0,0 +1,2 @@
+call hadoop fs -rm /AMPlayMaster*
+call hadoop fs -copyFromLocal AMPlayMaster/target/AMPlayMaster*.jar /
---
+++ runJob.cmd
@@ -0,0 +1,1 @@
+hadoop jar AMPlayClient/target/AMPlayClient-1.0-SNAPSHOT.jar com.shanyu.hadoop.client.AppClient
