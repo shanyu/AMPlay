208eeca3f9622f636fb3f80617968258bbfc52e6
Shanyu Zhao <shzhao@microsoft.com>
2013-10-15 15:48:56 -0700

    Use ping delay instead of powershell start-sleep cmdlet.
    Signed-off-by: Shanyu Zhao <shzhao@microsoft.com>

--- MyExecShell.cmd
+++ MyExecShell.cmd
@@ -1,3 +1,6 @@
 @echo start executing
-powershell start-sleep -s 30
+rem TBD: not sure why calling powershell doesn't work on HDInsight cluster
+rem      but it works fine on my onebox setup.
+rem powershell start-sleep -s 30
+ping /n 30 localhost
 @echo end executing
